b2bbc1c514581ad570c4ab49497eea36858c1286
Ciel85 <michaelwalter299@gmail.com>
2022-11-07 23:54:18 +0100

    Project Init, HomeScreen and CoinDetailedScreen in progress

---
+++ src/screens/CoinDetailedScreen/components/CoinDetailedHeader/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { Ionicons, EvilIcons } from "@expo/vector-icons";
+import styles from "./styles";
+
+
+const CoinDetailedHeader = (props) => {
+  const {image, symbol, marketCapRank} = props;
+  return (
+    <View style={styles.headerContainer}>
+      <Ionicons name="chevron-back-sharp" size={30} color="white" />
+      <View style={styles.ticketContainer}>
+        <Image source={{ uri: image }} style={{ width: 25, height: 25 }} />
+        <Text style={styles.ticketTitle}>{symbol.toUpperCase()}</Text>
+        <View style={styles.rankContainer}>
+          <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>
+            #{marketCapRank}
+          </Text>
+        </View>
+      </View>
+      <EvilIcons name="user" size={30} color="white" />
+    </View>
+  );
+};
+
+export default CoinDetailedHeader;
